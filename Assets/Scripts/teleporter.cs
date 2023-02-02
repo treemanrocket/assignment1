b1d49ce2a5479858c495c082213be699ed6c9890
Albert Cheung <albert52798@knights.ucf.edu>
2023-02-01 19:45:29 -0500

    Updated Harpoon and collectibles

--- Assets/Scripts/teleporter.cs
+++ Assets/Scripts/teleporter.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class teleporter : MonoBehaviour
 {
-
+    void Update()
+    {
+        // Rotate the game object that this script is attached to by 15 in the X axis,
+        // 30 in the Y axis and 45 in the Z axis, multiplied by deltaTime in order to make it per second
+        // rather than per frame.
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
